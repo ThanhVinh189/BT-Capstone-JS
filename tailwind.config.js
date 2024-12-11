3a7bc4293f7880ddfa9c0a29161ed58ae7650708
ThanhVinh189 <tham13734@gmail.com>
2024-12-11 21:19:22 +0700

    new file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,6 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
-    plugins: [require("flowbite/plugin")],
   },
+  plugins: [require("flowbite/plugin")],
 };
